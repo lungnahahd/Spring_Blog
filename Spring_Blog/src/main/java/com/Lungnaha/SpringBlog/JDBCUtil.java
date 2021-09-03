a781cc6611e06c6485f95fa710c5ee2ea71d0716
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-09-03 21:54:08 +0900

    Feat : "JDBCUtill" Dao 클래스에서 사용을 위해 JDBC 관련 클래스 추가

--- Spring_Blog/src/main/java/com/Lungnaha/SpringBlog/JDBCUtil.java
+++ Spring_Blog/src/main/java/com/Lungnaha/SpringBlog/JDBCUtil.java
@@ -1,18 +1,72 @@
 package com.Lungnaha.SpringBlog;
 
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import com.sun.jdi.connect.spi.Connection;
 
 // MyBatis 사용 전에 데이터베이스 연동 처리를 JDBC로 하기 위해서 이용되는 클래스
 // 이를 통해 Connection 획득과 해제 작업을 공통으로 처리
 public class JDBCUtil {
-	public static Connection getConnection{
+	public static void getConnection(){
 		try {
-			Class.forName("org.mysql.Driver"); // 확인이 필요한 부분
-			return DriverManager.getConnection(null)
+			Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	public static void close(PreparedStatement stmt, Connection conn) {
+		if (stmt != null) {
+			try {
+				if(!stmt.isClosed()) {
+					stmt.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				stmt = null;
+			}
+		}
+		if (conn != null) {
+			try {
+				if(!conn.isClosed()) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				conn=null;
+			}
+		}
+	}
+	public static void close(ResultSet rs, PreparedStatement stmt, Connection conn) {
+		if(rs!= null) {
+			try {
+				if(!rs.isClosed()) rs.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				rs= null;
+			}
+		}
+		if(stmt != null) {
+			try {
+				if(!stmt.isClosed()) stmt.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				stmt = null;
+			}
+		}
+		if(conn != null) {
+			try {
+				if(!conn.isClosed()) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				conn = null;
+			}
 		}
 	}
 }
