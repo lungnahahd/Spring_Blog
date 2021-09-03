3288338e722dd650aa4e178f07345044c789e027
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-09-03 22:29:28 +0900

    Feat : "DAO Insert" 글 등록 기능 구현

--- Spring_Blog/src/main/java/com/Lungnaha/SpringBlog/SpringBlogDAO.java
+++ Spring_Blog/src/main/java/com/Lungnaha/SpringBlog/SpringBlogDAO.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 
 import org.springframework.stereotype.Repository;
 
-import com.sun.jdi.connect.spi.Connection;
+import java.sql.Connection;
 
 // VO 클래스를 이용해서 CRUD 기능을 수행할 저장소 역할을 하는 클래스
 @Repository("blogDAO")
@@ -21,4 +21,23 @@ public class SpringBlogDAO {
 	private final String deleteString = "delete springblog where id=?";
 	private final String getString ="select * from springblog where id=?";
 	private final String listString = "select * from springblog order by id desc";
+	
+	// CRUD 기능을 수행하는 메소드 구현
+	// 글 등록 메소드
+	public void insertBlog(SpringBlogVO vo) {
+		System.out.println("===>글 등록 시작");
+		try {
+			conn = JDBCUtil.getConnection();
+			stmt = conn.prepareStatement(insertString);
+			// 글 등록을 위해서 VO 클래스에서 정보를 받아오는 부분
+			stmt.setString(1, vo.getTitle());
+			stmt.setString(2, vo.getWriter());
+			stmt.setString(3, vo.getContent());
+			stmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			JDBCUtil.close(stmt, conn);
+		}
+	}
 }
