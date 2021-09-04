c0895886bcbf8261590d65f8655f39c82dabcb82
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-09-04 22:30:37 +0900

    Feat : "UserVO" 멤버 관리와 관련된 VO 클래스

---
+++ Spring_Blog/src/main/java/com/Lungnaha/SpringBlog/UserVO.java
@@ -0,0 +1,36 @@
+package com.Lungnaha.SpringBlog;
+
+// 멤버 관리와 관련된 VO 클래스
+public class UserVO {
+	
+	private String id;
+	private String name;
+	private String role;
+	private String password;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
